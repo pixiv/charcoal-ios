eb52dd9db29b145d3ebaf7e2c0de113d37d0ef3a
kwzr <kwzr.poyo@gmail.com>
2023-05-17 14:21:41 +0900

    SwiftUI版 DeploymentTarget 16.0のプロジェクトを追加

---
+++ Examples/SwiftUISampleIos16/SwiftUISampleIos16/SwiftUISampleIos16App.swift
@@ -0,0 +1,11 @@
+import CharcoalSwiftUISample
+import SwiftUI
+
+@main
+struct SwiftUISampleIos16App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
