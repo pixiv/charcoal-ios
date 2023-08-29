b526824c0329a8313015fa57a875205bd5745472
Kevin <kevin@pixiv.co.jp>
2023-08-29 15:57:18 +0900

    Revert "use CTFont to calculate lineHeight"
    This reverts commit 058bf33084ea2aed795418b384683bb1ad99ae57.

--- Sources/CharcoalSwiftUI/Components/Typographies/FontModifiers.swift
+++ Sources/CharcoalSwiftUI/Components/Typographies/FontModifiers.swift
@@ -2,22 +2,16 @@ import SwiftUI
 
 struct CharcoalFontModifier: ViewModifier {
     let size: CGFloat
-    let weight: Font.Weight
+    let weight: UIFont.Weight
     let lineHeight: CGFloat
     let isSingleLine: Bool
 
     func body(content: Content) -> some View {
-        var fontLineHeight = size
-        let font = Font.system(size: size, design: .default).weight(weight)
-        
-        if let defalutFont = CTFontCreateUIFontForLanguage(.system, size, nil) {
-            fontLineHeight = defalutFont.lineHeight
-        }
-       
+        let font: UIFont = .systemFont(ofSize: size, weight: weight)
         return content
-            .font(font)
-            .lineSpacing(isSingleLine ? 0 : lineHeight - fontLineHeight)
-            .padding(.vertical, isSingleLine ? 0 : (lineHeight - fontLineHeight) / 2)
+            .font(Font(font))
+            .lineSpacing(isSingleLine ? 0 : lineHeight - font.lineHeight)
+            .padding(.vertical, isSingleLine ? 0 : (lineHeight - font.lineHeight) / 2)
             .lineLimit(isSingleLine ? 1 : nil)
             .fixedSize(horizontal: false, vertical: true)
     }
@@ -25,30 +19,12 @@ struct CharcoalFontModifier: ViewModifier {
 
 struct CharcoalMonoFontModifier: ViewModifier {
     let size: CGFloat
-    let weight: Font.Weight
+    let weight: UIFont.Weight
 
     func body(content: Content) -> some View {
-        let font = Font.system(size: size, design: .monospaced).weight(weight)
-        
+        let font: UIFont = .monospacedSystemFont(ofSize: size, weight: weight)
         return content
-            .font(font)
+            .font(Font(font))
             .lineLimit(1)
-    }
-}
-
-extension CTFont {
-    var lineHeight: CGFloat {
-        var lineHeight: CGFloat = 0.0
-     
-        // Get the ascent from the font, already scaled for the font's size
-        lineHeight += CTFontGetAscent(self)
-     
-        // Get the descent from the font, already scaled for the font's size
-        lineHeight += CTFontGetDescent(self)
-     
-        // Get the leading from the font, already scaled for the font's size
-        lineHeight += CTFontGetLeading(self)
-     
-        return lineHeight;
     }
 }
