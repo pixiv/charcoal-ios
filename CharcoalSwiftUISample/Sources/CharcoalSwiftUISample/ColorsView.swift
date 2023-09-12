a82c45e1f0109c102e62a1b7aee66bc72c2b1aa3
Kevin <kevin@pixiv.co.jp>
2023-09-12 17:45:38 +0900

    Update ColorsView.swift

--- CharcoalSwiftUISample/Sources/CharcoalSwiftUISample/ColorsView.swift
+++ CharcoalSwiftUISample/Sources/CharcoalSwiftUISample/ColorsView.swift
@@ -1,7 +1,7 @@
 import Charcoal
 import SwiftUI
 
-struct ColorsView: View {    
+struct ColorsView: View {
     let colors = [
         CharcoalAsset.ColorPaletteGenerated.background1,
         CharcoalAsset.ColorPaletteGenerated.background2,
