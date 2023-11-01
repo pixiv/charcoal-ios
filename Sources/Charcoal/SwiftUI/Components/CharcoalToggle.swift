59a54eef9e1cd4c049fd75896229586047075c34
Kevin <kevin@pixiv.co.jp>
2023-11-01 17:17:41 +0800

    Fix toggle's cornerCurve with continuous

--- Sources/Charcoal/SwiftUI/Components/CharcoalToggle.swift
+++ Sources/Charcoal/SwiftUI/Components/CharcoalToggle.swift
@@ -21,6 +21,7 @@ struct CharcoalToggleWrapper: UIViewRepresentable {
         uiView.onTintColor = CharcoalAsset.ColorPaletteGenerated.brand.color
         uiView.backgroundColor = CharcoalAsset.ColorPaletteGenerated.surface4.color
         uiView.layer.cornerRadius = uiView.frame.size.height / 2.0
+        uiView.layer.cornerCurve = .continuous
         uiView.isOn = isOn.wrappedValue
     }
 
