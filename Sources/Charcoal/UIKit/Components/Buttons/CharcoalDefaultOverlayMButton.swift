15b7177e63a196860f549c0f1c0799b5d816ac19
Kevin <kevin@pixiv.co.jp>
2023-11-08 12:57:26 +0800

    Update CharcoalDefaultOverlayMButton.swift

--- Sources/Charcoal/UIKit/Components/Buttons/CharcoalDefaultOverlayMButton.swift
+++ Sources/Charcoal/UIKit/Components/Buttons/CharcoalDefaultOverlayMButton.swift
@@ -1,8 +1,6 @@
 import UIKit
 
-@IBDesignable
 public class CharcoalDefaultOverlayMButton: UIButton, CharcoalButton {
-    @IBInspectable
     public var isFixed: Bool = false
 
     override public var intrinsicContentSize: CGSize {
@@ -32,11 +30,6 @@ public class CharcoalDefaultOverlayMButton: UIButton, CharcoalButton {
 
     override public func awakeFromNib() {
         super.awakeFromNib()
-        setupStyle()
-    }
-
-    override public func prepareForInterfaceBuilder() {
-        super.prepareForInterfaceBuilder()
         setupStyle()
     }
 
@@ -70,4 +63,12 @@ public class CharcoalDefaultOverlayMButton: UIButton, CharcoalButton {
     private func updateStyle() {
         alpha = isEnabled ? 1.0 : 0.32
     }
+}
+
+
+@available(iOS 17.0, *)
+#Preview {
+    let button = CharcoalDefaultOverlayMButton()
+    button.setTitle("フォロー中", for: .normal)
+    return button
 }
