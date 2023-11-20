0b8b66989ddba5403f12e6c300e40dcc08b814ab
Kevin <kevin@pixiv.co.jp>
2023-11-20 17:39:12 +0800

    Rename StoryboardTitles as ComponentTitles

--- CharcoalUIKitSample/Sources/CharcoalUIKitSample/ContentViewController.swift
+++ CharcoalUIKitSample/Sources/CharcoalUIKitSample/ContentViewController.swift
@@ -7,13 +7,30 @@ public final class ContentViewController: UIViewController {
         return view
     }()
     
-    enum StoryboardTitles: String, CaseIterable {
+    private enum ComponentTitles: String, CaseIterable {
         case buttons = "Buttons"
         case textFields = "TextFields"
         case selections = "Selections"
         case colors = "Colors"
         case typographies = "Typographies"
         case icons = "Icons"
+        
+        var viewController: UIViewController {
+            switch self {
+            case .typographies:
+                return TypographiesViewController()
+            case .buttons:
+                return ButtonsViewController()
+            case .colors:
+                return ColorsViewController()
+            case .icons:
+                return IconsViewController()
+            case .selections:
+                return SelectionsViewController()
+            case .textFields:
+                return TextFieldsViewController()
+            }
+        }
     }
     
     override public func viewDidLoad() {
@@ -57,7 +74,7 @@ extension ContentViewController: UITableViewDelegate, UITableViewDataSource {
         
         let cellIdentifier = "Cell"
         let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier) ?? UITableViewCell(style: .default, reuseIdentifier: cellIdentifier)
-        let title = StoryboardTitles.allCases[indexPath.row].rawValue
+        let title = ComponentTitles.allCases[indexPath.row].rawValue
         
         cell.textLabel?.text = title
         
@@ -65,13 +82,13 @@ extension ContentViewController: UITableViewDelegate, UITableViewDataSource {
     }
     
     public func tableView(_: UITableView, numberOfRowsInSection _: Int) -> Int {
-        return StoryboardTitles.allCases.count
+        return ComponentTitles.allCases.count
     }
     
     public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
         
-        let titleCase = StoryboardTitles.allCases[indexPath.item]
+        let titleCase = ComponentTitles.allCases[indexPath.item]
         let viewController = titleCase.viewController
         viewController.title = titleCase.rawValue
         navigationController?.pushViewController(viewController, animated: true)
@@ -79,25 +96,6 @@ extension ContentViewController: UITableViewDelegate, UITableViewDataSource {
     
     public func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         return "UI LIBRARY"
-    }
-}
-
-extension ContentViewController.StoryboardTitles {
-    var viewController: UIViewController {
-        switch self {
-        case .typographies:
-            return TypographiesViewController()
-        case .buttons:
-            return ButtonsViewController()
-        case .colors:
-            return ColorsViewController()
-        case .icons:
-            return IconsViewController()
-        case .selections:
-            return SelectionsViewController()
-        case .textFields:
-            return TextFieldsViewController()
-        }
     }
 }
 
