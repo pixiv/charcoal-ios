9f43621bf8b90be06a89ef6dedca9f430899469c
Kevin <kevin@pixiv.co.jp>
2023-11-22 14:33:33 +0800

    Refactor textfield

--- Sources/Charcoal/SwiftUI/Components/Typographies/FontModifiers.swift
+++ Sources/Charcoal/SwiftUI/Components/Typographies/FontModifiers.swift
@@ -5,13 +5,23 @@ struct CharcoalFontModifier: ViewModifier {
     let weight: UIFont.Weight
     let lineHeight: CGFloat
     let isSingleLine: Bool
+    
+    @Environment(\.sizeCategory) var sizeCategory
+    
+    var scaledFontSize: CGFloat {
+        return UIFontMetrics.default.scaledValue(for: size)
+    }
+    
+    var scaledLineHeight: CGFloat {
+        return UIFontMetrics.default.scaledValue(for: lineHeight)
+    }
 
     func body(content: Content) -> some View {
-        let font: UIFont = .systemFont(ofSize: size, weight: weight)
+        let font: UIFont = .systemFont(ofSize: scaledFontSize, weight: weight)
         return content
             .font(Font(font))
-            .lineSpacing(isSingleLine ? 0 : lineHeight - font.lineHeight)
-            .padding(.vertical, isSingleLine ? 0 : (lineHeight - font.lineHeight) / 2)
+            .lineSpacing(isSingleLine ? 0 : scaledLineHeight - font.lineHeight)
+            .padding(.vertical, isSingleLine ? 0 : (scaledLineHeight - font.lineHeight) / 2)
             .lineLimit(isSingleLine ? 1 : nil)
             .fixedSize(horizontal: false, vertical: true)
     }
@@ -20,9 +30,13 @@ struct CharcoalFontModifier: ViewModifier {
 struct CharcoalMonoFontModifier: ViewModifier {
     let size: CGFloat
     let weight: UIFont.Weight
+    
+    var scaledFontSize: CGFloat {
+        return UIFontMetrics.default.scaledValue(for: size)
+    }
 
     func body(content: Content) -> some View {
-        let font: UIFont = .monospacedSystemFont(ofSize: size, weight: weight)
+        let font: UIFont = .monospacedSystemFont(ofSize: scaledFontSize, weight: weight)
         return content
             .font(Font(font))
             .lineLimit(1)
