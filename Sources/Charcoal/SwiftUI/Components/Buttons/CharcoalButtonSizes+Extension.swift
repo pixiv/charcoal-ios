5fe54673fe7666d1b30bcbd238b46a32f1f3f97f
Kevin <kevin@pixiv.co.jp>
2023-12-12 16:35:12 +0900

    Update edge insets

--- Sources/Charcoal/SwiftUI/Components/Buttons/CharcoalButtonSizes+Extension.swift
+++ Sources/Charcoal/SwiftUI/Components/Buttons/CharcoalButtonSizes+Extension.swift
@@ -4,9 +4,9 @@ extension CharcoalButtonSize {
     var padding: EdgeInsets {
         switch self {
         case .medium:
-            return EdgeInsets(top: 11, leading: 24, bottom: 11, trailing: 24)
+            return EdgeInsets(top: 11.5, leading: 24, bottom: 11.5, trailing: 24)
         case .small:
-            return EdgeInsets(top: 7, leading: 16, bottom: 7, trailing: 16)
+            return EdgeInsets(top: 7.5, leading: 16, bottom: 7.5, trailing: 16)
         }
     }
     
