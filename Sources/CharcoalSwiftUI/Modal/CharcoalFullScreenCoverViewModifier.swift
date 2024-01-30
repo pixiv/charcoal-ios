7a2723cbf960eca6a7506b504d8dbc2682a96531
Kevin <141606011+kevinneko@users.noreply.github.com>
2024-01-30 15:04:16 +0900

    Refactor SwiftUI Modal with CharcoalFullScreenCoverViewModifier (#189)
    * Refactor code and fix the model responsiveness delay after dismiss
    
    * Format Code
    
    * Refactor the magic code to be more readable
    
    * Fix unexpected animation on iPadOS
    
    * Refactor with CharcoalFullScreenCoverViewModifier
    
    * Give indicatorInset a better bottom margin
    
    * Reformat
    
    * Add duration and maxWidth to modal modifier
    
    * Refactor Modal View
    
    * Set hostingViewController as state
    
    * Reformat
    
    * Rename modalSize as offset
    
    * Refactor comment
    
    * Fix windowScene keyWindow logic
    
    * Fix UIWindowScene finding logic

---
+++ Sources/CharcoalSwiftUI/Modal/CharcoalFullScreenCoverViewModifier.swift
@@ -0,0 +1,49 @@
+import SwiftUI
+
+struct CharcoalFullScreenCoverViewModifier<SubContent: View>: ViewModifier {
+    @Binding var isPresented: Bool
+
+    @ViewBuilder var subContent: () -> SubContent
+
+    var duration: Double
+
+    @State var hostingViewController: UIHostingController<SubContent>? = nil
+
+    init(isPresented: Binding<Bool>, duration: Double, @ViewBuilder subContent: @escaping () -> SubContent) {
+        _isPresented = isPresented
+        self.subContent = subContent
+        self.duration = duration
+    }
+
+    func body(content: Content) -> some View {
+        content
+            .onChange(of: isPresented) { newValue in
+                let scene = UIApplication.shared.connectedScenes.compactMap { $0 as? UIWindowScene }
+                    .filter { $0.activationState == .foregroundActive }
+                    .first
+                if let windowScene = scene {
+                    if newValue {
+                        hostingViewController = UIHostingController(rootView: subContent())
+
+                        if let hostingViewController {
+                            hostingViewController.view.backgroundColor = UIColor.clear
+                            hostingViewController.modalPresentationStyle = .overFullScreen
+                            windowScene.windows.filter { $0.isKeyWindow }.first?.rootViewController?.present(hostingViewController, animated: false)
+                        }
+                    } else {
+                        Task {
+                            // Wait for the dismiss animation to finish
+                            try await Task.sleep(nanoseconds: UInt64(self.duration * 1000) * 1000000)
+                            hostingViewController?.dismiss(animated: false)
+                        }
+                    }
+                }
+            }
+    }
+}
+
+extension View {
+    func charcoalFullScreenCover<SubContent: View>(isPresented: Binding<Bool>, duration: Double = 0.25, @ViewBuilder content: @escaping () -> SubContent) -> some View {
+        modifier(CharcoalFullScreenCoverViewModifier(isPresented: isPresented, duration: duration, subContent: content))
+    }
+}
