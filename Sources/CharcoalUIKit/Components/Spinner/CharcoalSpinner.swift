6528b6a2a14424945f1193a0c4c8e4f09cd8bc28
Kevin <kevin@pixiv.co.jp>
2024-03-31 15:48:08 +0900

    Can get mainWindow from parameter or a guess

--- Sources/CharcoalUIKit/Components/Spinner/CharcoalSpinner.swift
+++ Sources/CharcoalUIKit/Components/Spinner/CharcoalSpinner.swift
@@ -1,9 +1,24 @@
 import UIKit
 
 public class CharcoalSpinner: UIView {
-    var main: UIWindow!
+    var mainWindow: UIWindow!
 }
 
+extension CharcoalSpinner {
+    /// Initializes the spinner with the given window.
+    func setupWindow(window: UIWindow?) {
+        if let window = window {
+            mainWindow = window
+        } else {
+            if (mainWindow == nil) {
+                let scene = UIApplication.shared.connectedScenes.compactMap { $0 as? UIWindowScene }
+                                    .filter { $0.activationState == .foregroundActive }
+                                    .first
+                mainWindow = scene?.windows.filter { $0.isKeyWindow }.first ?? UIApplication.shared.windows.first
+            }
+        }
+    }
+}
 
 @available(iOS 17.0, *)
 #Preview {
