eb25b09728946573586c789efdce23cfc5999ef2
Kevin <kevin@pixiv.co.jp>
2024-05-15 16:56:38 +0800

    Add toasts example

--- CharcoalUIKitSample/Sources/CharcoalUIKitSample/ContentViewController.swift
+++ CharcoalUIKitSample/Sources/CharcoalUIKitSample/ContentViewController.swift
@@ -50,6 +50,7 @@ public final class ContentViewController: UIViewController {
         case typographies = "Typographies"
         case icons = "Icons"
         case tooltips = "Tooltips"
+        case toasts = "Toasts"
 
         var viewController: UIViewController {
             switch self {
@@ -67,6 +68,8 @@ public final class ContentViewController: UIViewController {
                 return TextFieldsViewController()
             case .tooltips:
                 return TooltipsViewController()
+            case .toasts:
+                return ToastsViewController()
             }
         }
     }
