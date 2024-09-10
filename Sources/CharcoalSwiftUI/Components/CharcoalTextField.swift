f5c671070279e1c7d13152776a0d5e479b4a30d8
Kevin <kevin@pixiv.co.jp>
2024-09-10 15:49:51 +0900

    Revert "Clean Xcode 16 warning"
    This reverts commit 7168c655bf805ecb18615eacb48c3886daed3498.

--- Sources/CharcoalSwiftUI/Components/CharcoalTextField.swift
+++ Sources/CharcoalSwiftUI/Components/CharcoalTextField.swift
@@ -90,42 +90,35 @@ public extension View {
 }
 
 @available(iOS 15, *)
-struct CharcoalTextFieldPreview: View {
+#Preview {
     @State var text1 = ""
     @State var text2 = ""
-    
-    var body: some View {
-        VStack(spacing: 16) {
-            TextField("Simple text field", text: $text1)
-                .charcoalTextField()
-            TextField("Placeholder", text: $text2)
-                .charcoalTextField(
-                    label: .constant("Label"),
-                    countLabel: .init(
-                        get: { "\(text2.count)/10" },
-                        set: { _ in }
-                    ),
-                    assistiveText: .init(
-                        get: { text2.count > 10 ? "Error" : "OK" },
-                        set: { _ in }
-                    ),
-                    hasError: .init(
-                        get: { text2.count > 10 },
-                        set: { _ in }
-                    )
-                )
-            TextField("", text: .constant("Text"))
-                .disabled(true)
-                .charcoalTextField(
-                    label: .constant("Label"),
-                    countLabel: .constant("0/10"),
-                    assistiveText: .constant("Assistive text")
-                )
-        }.padding()
-    }
-}
 
-@available(iOS 15, *)
-#Preview {
-    CharcoalTextFieldPreview()
+    return VStack(spacing: 16) {
+        TextField("Simple text field", text: $text1)
+            .charcoalTextField()
+        TextField("Placeholder", text: $text2)
+            .charcoalTextField(
+                label: .constant("Label"),
+                countLabel: .init(
+                    get: { "\(text2.count)/10" },
+                    set: { _ in }
+                ),
+                assistiveText: .init(
+                    get: { text2.count > 10 ? "Error" : "OK" },
+                    set: { _ in }
+                ),
+                hasError: .init(
+                    get: { text2.count > 10 },
+                    set: { _ in }
+                )
+            )
+        TextField("", text: .constant("Text"))
+            .disabled(true)
+            .charcoalTextField(
+                label: .constant("Label"),
+                countLabel: .constant("0/10"),
+                assistiveText: .constant("Assistive text")
+            )
+    }.padding()
 }
