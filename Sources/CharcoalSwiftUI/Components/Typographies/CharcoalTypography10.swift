29d23d6f08c35a0c58cdd3c36f441e19ed72a753
Kevin <kevin@pixiv.co.jp>
2024-09-10 15:38:18 +0900

    Reformat

--- Sources/CharcoalSwiftUI/Components/Typographies/CharcoalTypography10.swift
+++ Sources/CharcoalSwiftUI/Components/Typographies/CharcoalTypography10.swift
@@ -1,22 +1,18 @@
 import SwiftUI
 
 public extension View {
-    @warn_unqualified_access
     func charcoalTypography10Bold(isSingleLine: Bool = false) -> some View {
         return modifier(CharcoalFontModifier(size: 10, weight: .bold, lineHeight: 18, isSingleLine: isSingleLine, textStyle: .body))
     }
 
-    @warn_unqualified_access
     func charcoalTypography10Regular(isSingleLine: Bool = false) -> some View {
         return modifier(CharcoalFontModifier(size: 10, weight: .regular, lineHeight: 18, isSingleLine: isSingleLine, textStyle: .body))
     }
 
-    @warn_unqualified_access
     func charcoalTypography10BoldMono() -> some View {
         return modifier(CharcoalMonoFontModifier(size: 10, weight: .bold, textStyle: .body))
     }
 
-    @warn_unqualified_access
     func charcoalTypography10RegularMono() -> some View {
         return modifier(CharcoalMonoFontModifier(size: 10, weight: .regular, textStyle: .body))
     }
--- Sources/CharcoalSwiftUI/Components/Typographies/CharcoalTypograpy20.swift
+++ Sources/CharcoalSwiftUI/Components/Typographies/CharcoalTypograpy20.swift
@@ -4,7 +4,6 @@ private let fontSize = CGFloat(charcoalFoundation.typography.size.the20.fontSize
 private let lineHeight = CGFloat(charcoalFoundation.typography.size.the20.lineHeight)
 
 public extension View {
-    @warn_unqualified_access
     func charcoalTypography20Bold(isSingleLine: Bool = false) -> some View {
         return modifier(CharcoalFontModifier(
             size: fontSize,
@@ -14,7 +13,6 @@ public extension View {
         ))
     }
 
-    @warn_unqualified_access
     func charcoalTypography20Regular(isSingleLine: Bool = false) -> some View {
         return modifier(CharcoalFontModifier(
             size: fontSize,
@@ -24,7 +22,6 @@ public extension View {
         ))
     }
 
-    @warn_unqualified_access
     func charcoalTypography20BoldMono() -> some View {
         return modifier(CharcoalMonoFontModifier(
             size: fontSize,
@@ -32,7 +29,6 @@ public extension View {
         ))
     }
 
-    @warn_unqualified_access
     func charcoalTypography20RegularMono() -> some View {
         return modifier(CharcoalMonoFontModifier(
             size: fontSize,
