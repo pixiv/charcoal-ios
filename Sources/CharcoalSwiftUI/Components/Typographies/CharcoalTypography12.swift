caf4916307e9a15bf521886cd5acc811c22de25d
Kevin <kevin@pixiv.co.jp>
2024-12-26 16:51:57 +0900

    Prevent from Japanese size adjust

--- Sources/CharcoalSwiftUI/Components/Typographies/CharcoalTypography12.swift
+++ Sources/CharcoalSwiftUI/Components/Typographies/CharcoalTypography12.swift
@@ -40,3 +40,27 @@ public extension View {
         ))
     }
 }
+
+#Preview {
+    VStack {
+        Text("Appleでの日本語Mix深圳")
+            .font(.custom(".AppleSystemUIFont", size: 12))
+            
+        VStack(alignment: .leading, spacing: 4) {
+            Text("Appleでの日本語Mix深圳")
+                .charcoalTypography12Regular(isSingleLine: false)
+            
+            Text("Appleでの日本語Mix深圳")
+                .charcoalTypography12Bold(isSingleLine: false)
+            
+            Text("Appleでの日本語Mix深圳")
+                .charcoalTypography12BoldMono()
+            
+            Text("Appleでの日本語Mix深圳")
+                .charcoalTypography12RegularMono()
+            
+            Text("Appleでの日本語Mix深圳")
+                .font(.custom("Hiragino Sans", size: 12))
+        }.background(charcoalColor: .background2)
+    }
+}
--- Sources/CharcoalSwiftUI/Components/Typographies/FontModifiers.swift
+++ Sources/CharcoalSwiftUI/Components/Typographies/FontModifiers.swift
@@ -20,8 +20,16 @@ struct CharcoalFontModifier: ViewModifier {
 
     func body(content: Content) -> some View {
         let font: UIFont = .systemFont(ofSize: fontSize, weight: weight)
+        var attributes: [UIFontDescriptor.AttributeName : Any] = [:]
+        var traits:  [UIFontDescriptor.TraitKey: Any] = [:]
+        traits[.weight] = weight
+        attributes[UIFontDescriptor.AttributeName.name] = nil
+        attributes[UIFontDescriptor.AttributeName.family] = "Hiragino Sans"
+        attributes[UIFontDescriptor.AttributeName.traits] = traits
+        let fallbackFont = UIFontDescriptor(fontAttributes: attributes)
+        let repaired = font.fontDescriptor.addingAttributes([UIFontDescriptor.AttributeName.cascadeList : [fallbackFont]])
         return content
-            .font(Font(font))
+            .font(Font(UIFont(descriptor: repaired, size: size)))
             .lineSpacing(isSingleLine ? 0 : lineHeight - font.lineHeight)
             .padding(.vertical, isSingleLine ? 0 : (lineHeight - font.lineHeight) / 2)
             .lineLimit(isSingleLine ? 1 : nil)
@@ -45,8 +53,16 @@ struct CharcoalMonoFontModifier: ViewModifier {
 
     func body(content: Content) -> some View {
         let font: UIFont = .monospacedSystemFont(ofSize: fontSize, weight: weight)
+        var attributes: [UIFontDescriptor.AttributeName : Any] = [:]
+        var traits: [UIFontDescriptor.TraitKey: Any] = [:]
+        traits[.weight] = weight
+        attributes[UIFontDescriptor.AttributeName.name] = nil
+        attributes[UIFontDescriptor.AttributeName.family] = "Hiragino Sans"
+        attributes[UIFontDescriptor.AttributeName.traits] = traits
+        let fallbackFont = UIFontDescriptor(fontAttributes: attributes)
+        let repaired = font.fontDescriptor.addingAttributes([UIFontDescriptor.AttributeName.cascadeList : [fallbackFont]])
         return content
-            .font(Font(font))
+            .font(Font(UIFont(descriptor: repaired, size: size)))
             .lineLimit(1)
     }
 }
