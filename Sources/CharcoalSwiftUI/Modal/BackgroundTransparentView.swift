d95bd6ed6f9a4bb2eb3e03b9b3322f0453d03cbf
Atsuya Sato <natmark0918@gmail.com>
2025-05-21 16:36:09 +0900

    charcoalModalの表示をfullScreenCoverで行うようにしました (#322)
    * charcoalModalの表示をfullScreenCoverで行うようにしました
    
    * next responderを辿らずにUIHostingViewに対してnextを呼ぶようにした
    
    * nextResponderの探索処理をwhileループに戻した
    
    * BackgroundTransparentViewの実装をDispatchQueue.main.asyncの代わりにlayoutSubviewsで設定するようにした

--- Sources/CharcoalSwiftUI/Modal/BackgroundTransparentView.swift
+++ Sources/CharcoalSwiftUI/Modal/BackgroundTransparentView.swift
@@ -1,16 +1,34 @@
 import SwiftUI
 
 struct BackgroundTransparentView: UIViewRepresentable {
-    func makeUIView(context _: Context) -> UIView {
-        TransparentView()
+    func makeUIView(context: Context) -> UIView {
+        let view = TransparentView()
+        return view
     }
 
-    func updateUIView(_: UIView, context _: Context) {}
+    func updateUIView(_ uiView: UIView, context: Context) {}
+}
 
-    private class TransparentView: UIView {
-        override func layoutSubviews() {
-            super.layoutSubviews()
-            superview?.superview?.backgroundColor = .clear
+private class TransparentView: UIView {
+    override func layoutSubviews() {
+        super.layoutSubviews()
+
+        // viewが紐づくUIHostingViewの背景色を透過させることでfullScreenCoverで表示した際に背景を透過させられる
+        // SwiftUI._UIHostingView<SwiftUI.AnyView>
+        superview?.superview?.backgroundColor = UIColor.black.withAlphaComponent(0.2)
+
+        // SwiftUI.PresentationHostingController<SwiftUI.AnyView> を取り出す
+        // iOS 17以前は SwiftUI._UIHostingView<SwiftUI.AnyView>のnextResponderはSwiftUI.PresentationHostingControllerになっているが
+        // iOS 18以降だと SwiftUI._UIHostingView<SwiftUI.AnyView> -> SwiftUI.UIKitKeyPressResponder -> SwiftUI.PresentationHostingController になるため、whileループをしている
+        var responder: UIResponder? = superview?.superview?.next
+        while responder != nil {
+            guard let responder, let vc = responder as? UIViewController else {
+                responder = responder?.next
+                continue
+            }
+            // viewが紐づいているPresentationHostingControllerのmodalTransitionStyleをcrossDissolveにしておくことで、fullScreenCoverを閉じるときのアニメーションを標準のslideから変更できる
+            vc.modalTransitionStyle = .crossDissolve
+            break
         }
     }
 }
