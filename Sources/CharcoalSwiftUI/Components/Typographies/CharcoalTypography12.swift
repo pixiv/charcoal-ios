1df30589ee3fe06224219d90941f8b2cf5ed4509
Tengu712 <65766435+Tengu712@users.noreply.github.com>
2025-07-11 17:33:01 +0900

    iOS 26由来のcharcoalTypographyにおける名前衝突を修正 (#337)
    Co-authored-by: tengu712 <tengu712@pixiv.co.jp>

--- Sources/CharcoalSwiftUI/Components/Typographies/CharcoalTypography12.swift
+++ Sources/CharcoalSwiftUI/Components/Typographies/CharcoalTypography12.swift
@@ -1,14 +1,14 @@
 import SwiftUI
 
-private let fontSize = CGFloat(charcoalFoundation.typography.size.the12.fontSize)
-private let lineHeight = CGFloat(charcoalFoundation.typography.size.the12.lineHeight)
+private let the12FontSize = CGFloat(charcoalFoundation.typography.size.the12.fontSize)
+private let the12LineHeight = CGFloat(charcoalFoundation.typography.size.the12.lineHeight)
 
 public extension View {
     func charcoalTypography12Bold(isSingleLine: Bool = false) -> some View {
         return modifier(CharcoalFontModifier(
-            size: fontSize,
+            size: the12FontSize,
             weight: .bold,
-            lineHeight: lineHeight,
+            lineHeight: the12LineHeight,
             isSingleLine: isSingleLine,
             textStyle: .body
         ))
@@ -16,9 +16,9 @@ public extension View {
 
     func charcoalTypography12Regular(isSingleLine: Bool = false) -> some View {
         return modifier(CharcoalFontModifier(
-            size: fontSize,
+            size: the12FontSize,
             weight: .regular,
-            lineHeight: lineHeight,
+            lineHeight: the12LineHeight,
             isSingleLine: isSingleLine,
             textStyle: .body
         ))
@@ -26,7 +26,7 @@ public extension View {
 
     func charcoalTypography12BoldMono() -> some View {
         return modifier(CharcoalMonoFontModifier(
-            size: fontSize,
+            size: the12FontSize,
             weight: .bold,
             textStyle: .body
         ))
@@ -34,7 +34,7 @@ public extension View {
 
     func charcoalTypography12RegularMono() -> some View {
         return modifier(CharcoalMonoFontModifier(
-            size: fontSize,
+            size: the12FontSize,
             weight: .regular,
             textStyle: .body
         ))
--- Sources/CharcoalSwiftUI/Components/Typographies/CharcoalTypography16.swift
+++ Sources/CharcoalSwiftUI/Components/Typographies/CharcoalTypography16.swift
@@ -1,37 +1,37 @@
 import SwiftUI
 
-private let fontSize = CGFloat(charcoalFoundation.typography.size.the16.fontSize)
-private let lineHeight = CGFloat(charcoalFoundation.typography.size.the16.lineHeight)
+private let the16FontSize = CGFloat(charcoalFoundation.typography.size.the16.fontSize)
+private let the16LineHeight = CGFloat(charcoalFoundation.typography.size.the16.lineHeight)
 
 public extension View {
     func charcoalTypography16Bold(isSingleLine: Bool = false) -> some View {
         return modifier(CharcoalFontModifier(
-            size: fontSize,
+            size: the16FontSize,
             weight: .bold,
-            lineHeight: lineHeight,
+            lineHeight: the16LineHeight,
             isSingleLine: isSingleLine
         ))
     }
 
     func charcoalTypography16Regular(isSingleLine: Bool = false) -> some View {
         return modifier(CharcoalFontModifier(
-            size: fontSize,
+            size: the16FontSize,
             weight: .regular,
-            lineHeight: lineHeight,
+            lineHeight: the16LineHeight,
             isSingleLine: isSingleLine
         ))
     }
 
     func charcoalTypography16BoldMono() -> some View {
         return modifier(CharcoalMonoFontModifier(
-            size: fontSize,
+            size: the16FontSize,
             weight: .bold
         ))
     }
 
     func charcoalTypography16RegularMono() -> some View {
         return modifier(CharcoalMonoFontModifier(
-            size: fontSize,
+            size: the16FontSize,
             weight: .regular
         ))
     }
