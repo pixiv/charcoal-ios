92a219cc50d7c1e767845dd480acf01c06227711
kwzr <kwzr@pixiv.co.jp>
2026-02-20 18:23:38 +0900

    Add UIColor(charcoalColor:) and refactor UIKit color usage

--- Sources/CharcoalSwiftUI/Extensions/UIColor+Extension.swift
+++ Sources/CharcoalSwiftUI/Extensions/UIColor+Extension.swift
@@ -1,6 +1,12 @@
 import UIKit
 
 extension UIColor {
+    public convenience init(charcoalColor: CharcoalAsset.ColorPaletteGenerated) {
+        self.init { traitCollection in
+            charcoalColor.colorAsset.color(compatibleWith: traitCollection)
+        }
+    }
+
     func imageWithColor(width: Int, height: Int) -> UIImage {
         let size = CGSize(width: width, height: height)
         return UIGraphicsImageRenderer(size: size).image { rendererContext in
